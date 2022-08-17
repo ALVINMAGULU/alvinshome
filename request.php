2871894a7232a50635696c4ba224ffb9c4938a76
ALVINMAGULU <66173915+ALVINMAGULU@users.noreply.github.com>
2022-08-17 05:37:35 +0300

    Update request.php

--- request.php
+++ request.php
@@ -81,10 +81,7 @@ mysqli_close($conn);
     if($access == true){
     $sql1 = "SELECT Security,Indoor,backlight,updateOTA,MotionSensor,LDR FROM mydb.homethings WHERE ID= '1'";
     $result = mysqli_query($conn,$sql1);
-    if($row = $result->fetch_assoc()){
-        print("worked");
-    }
-     
+    $row = $result->fetch_assoc()    
     $json = [];
     $json += ["homeDevice" => $homedevice];
     $json += ["Indoor" => booleaner($row["Indoor"])];
