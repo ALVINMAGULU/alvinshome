0b3fa9fc3d95af51dda285eae64d9a782d68461c
ALVINMAGULU <66173915+ALVINMAGULU@users.noreply.github.com>
2022-08-19 13:09:06 +0300

    Update request.php

--- request.php
+++ request.php
@@ -16,6 +16,24 @@ include ("dbconnect.php");
            return true;
        }
    }
+
+public static function convert_to_utf8($dat)
+   {
+      if (is_string($dat)) {
+         return utf8_encode($dat);
+      } elseif (is_array($dat)) {
+         $ret = [];
+         foreach ($dat as $i => $d) $ret[ $i ] = self::convert_to_utf8($d);
+
+         return $ret;
+      } elseif (is_object($dat)) {
+         foreach ($dat as $i => $d) $dat->$i = self::convert_to_utf8($d);
+
+         return $dat;
+      } else {
+         return $dat;
+      }
+   }
    
    
          
